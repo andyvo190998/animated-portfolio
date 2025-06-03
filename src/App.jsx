b39885a04d97c018905781173545ef80122e8b30
andyvo190998 <andyvo190998@gmail.com>
2025-06-03 16:33:30 +0200

    test pipeline

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,28 @@
-import { BrowserRouter } from 'react-router-dom';
-import {
-  About,
-  Contact,
-  Experience,
-  Navbar,
-  Tech,
-  Works,
-  Hero,
-  StarsCanvas,
-} from './components';
-import Education from './components/Education';
-
+import { BrowserRouter } from "react-router-dom";
+import { About, Contact, Experience, Navbar, Tech, Works, Hero, StarsCanvas } from "./components";
+import Education from "./components/Education";
 
 const App = () => {
-  return (
-    <BrowserRouter>
-      <div className="relative z-0 bg-primary">
-        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
-          <Navbar />
-          <Hero />
-        </div>
-        <About />
-        <Experience />
-        <Tech />
-        <Education />
-        <Works />
-        <div className="relative z-0">
-          <Contact />
-          <StarsCanvas />
-        </div>
-      </div>
-    </BrowserRouter>
-  );
+	console.log("App component rendered");
+	return (
+		<BrowserRouter>
+			<div className="relative z-0 bg-primary">
+				<div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+					<Navbar />
+					<Hero />
+				</div>
+				<About />
+				<Experience />
+				<Tech />
+				<Education />
+				<Works />
+				<div className="relative z-0">
+					<Contact />
+					<StarsCanvas />
+				</div>
+			</div>
+		</BrowserRouter>
+	);
 };
 
 export default App;
